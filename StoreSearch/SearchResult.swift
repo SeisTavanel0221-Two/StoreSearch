b8f7a16f30f3645ba09b7e318ae08d13c95fc7fa
SeisTavanel0221-Two <gjohns30@live.spcollege.edu>
2024-02-19 10:03:25 -0500

    Setup basic search bar and table user interface

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Gareth Johnson-Lewis on 2/19/24.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
